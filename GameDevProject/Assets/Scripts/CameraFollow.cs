ff7ae3c2350f0982343f7abbf7f72916573bf140
Kieran-git <kieranfoystudent@outlook.com>
2022-02-09 11:47:42 +0000

    Camera Follow

--- GameDevProject/Assets/Scripts/CameraFollow.cs
+++ GameDevProject/Assets/Scripts/CameraFollow.cs
@@ -4,6 +4,10 @@ using UnityEngine;
 
 public class CameraFollow : MonoBehaviour
 {
+    public Transform Player;
+    Vector3 TargetPosition;
+    public float FollowSpeed;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +17,18 @@ public class CameraFollow : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        Vector3 dirToPlayer = Player.position - transform.position;
+
+        dirToPlayer += new Vector3(0f, 4f, -9f);
+        dirToPlayer.Normalize();
+
+        transform.position += dirToPlayer * FollowSpeed * Time.deltaTime;
+
         
+/*
+        TargetPosition += transform.position - Player.position;
+
+        transform.position = Vector3.Lerp(transform.position, TargetPosition, FollowSpeed * Time.deltaTime);
+    */
     }
 }
