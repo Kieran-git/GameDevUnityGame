4368af1053c1ea2ccf225b388ffe86ec1883c2c1
Kieran-git <kieranfoystudent@outlook.com>
2022-02-16 12:01:18 +0000

    Create gamestate class

---
+++ GameDevProject/Assets/Scripts/GameState.cs
@@ -0,0 +1,5 @@
+﻿public static class GameState
+{
+    public static bool GamePaused;
+
+}
--- GameDevProject/Assets/Scripts/RockFall.cs
+++ GameDevProject/Assets/Scripts/RockFall.cs
@@ -13,7 +13,6 @@ public class RockFall : MonoBehaviour
             gameObject.AddComponent<Shrinking>();
             Collided = true;
         }
-
     }
 
     // Start is called before the first frame update
