fdb2b965d5d30682fcfe8623993a02beab02ac42
Kieran-git <64077345+Kieran-git@users.noreply.github.com>
2022-03-02 11:53:20 +0000

    Bug fix - Shrinking again before normal size
    Added a check before toggling shrinking to also check if the player is at the normal size.

--- GameDevProject/Assets/Scripts/PlayerController.cs
+++ GameDevProject/Assets/Scripts/PlayerController.cs
@@ -52,20 +52,27 @@ public class PlayerController : MonoBehaviour
         rb.velocity = v;
     }
 
+    // To Do: Add Movement & Jump speed scaling
     void ShrinkAndGrow()
     {
-        // If not already shrinking and Q is pressed toggle shrinking,
-        // otherwise if smaller size has been reached toggle growing
-        if (!Shrinking && Input.GetKeyDown(KeyCode.Q)) Shrinking = true;
-        if (transform.localScale.x <= SmallerSize) Shrinking = false;
+        // Toggle Shirnk - If not already shrinking and Q is pressed and at the normal size then toggle shrinking
+        if (!Shrinking && Input.GetKeyDown(KeyCode.Q) && transform.localScale.x >= NormalSize)
+        {
+            Shrinking = true;
+        }
+        // Toggle Grow - If smaller size has been reached toggle growing. 
+        if (transform.localScale.x <= SmallerSize)
+        {
+            Shrinking = false;
+        }
 
-        // Shrinking - while player isn't smaller size decrease until they are
+        // Shrinking - while player isn't smaller size decrease until they are, shrinking must be true
         if (Shrinking && transform.localScale.x > SmallerSize)
         {
             transform.localScale -= Vector3.one * ShrinkSpeed * Time.deltaTime;
         }
 
-        // Growing - while player isn't normal size increase until they are
+        // Growing - while player isn't normal size increase until they are, shrinking must be false
         if (!Shrinking && transform.localScale.x <= NormalSize)
         {
             transform.localScale += Vector3.one * GrowSpeed * Time.deltaTime;
